6d899f7105ddf0de92923feb7fe4de75b2025dab
lack-io <598223084@qq.com>
2022-08-25 10:23:28 +0800

    fix: api.meta

--- apis/meta/v1/meta.pb.validator.go
+++ apis/meta/v1/meta.pb.validator.go
@@ -5,6 +5,7 @@ package metav1
 
 import (
 	fmt "fmt"
+
 	is "github.com/vine-io/vine/util/is"
 )
 
@@ -44,8 +45,8 @@ func (m *State) Validate() error {
 func (m *State) ValidateE(prefix string) error {
 	errs := make([]error, 0)
 	if int32(m.Code) != 0 {
-		if !is.In([]int32{0, 1}, int32(m.Code)) {
-			errs = append(errs, fmt.Errorf("field '%scode' must in '[0, 1]'", prefix))
+		if !is.In([]int32{0, 1, 2}, int32(m.Code)) {
+			errs = append(errs, fmt.Errorf("field '%scode' must in '[0, 1, 2]'", prefix))
 		}
 	}
 	return is.MargeErr(errs...)
