d87ee2fbddd6fce69519a7b6c753798f2bd3f710
lack <598223084@qq.com>
2023-04-19 08:43:35 +0800

    fix: 调整 metav1

--- apis/meta/v1/types.go
+++ apis/meta/v1/types.go
@@ -42,18 +42,18 @@ const (
 // +gogo:gengorm=true
 type TypeMeta struct {
 	// 资源类型
-	Kind string `json:"kind,omitempty" protobuf:"bytes,1,opt,name=kind" gorm:"column:kind"`
+	Kind string `json:"kind,omitempty" protobuf:"bytes,1,opt,name=kind,proto3"`
 	// 资源版本信息
-	ApiVersion string `json:"apiVersion,omitempty" protobuf:"bytes,2,opt,name=apiVersion" gorm:"column:apiVersion"`
+	ApiVersion string `json:"apiVersion,omitempty" protobuf:"bytes,2,opt,name=apiVersion,proto3"`
 }
 
 // +gogo:deepcopy=true
 // +gogo:genproto=true
 type OwnerReference struct {
-	ApiVersion string `json:"apiVersion,omitempty" protobuf:"bytes,1,opt,name=apiVersion"`
-	Kind       string `json:"kind,omitempty" protobuf:"bytes,2,opt,name=kind"`
-	Name       string `json:"name,omitempty" protobuf:"bytes,3,opt,name=name"`
-	Uid        string `json:"uid,omitempty" protobuf:"bytes,4,opt,name=uid"`
+	ApiVersion string `json:"apiVersion,omitempty" protobuf:"bytes,1,opt,name=apiVersion,proto3"`
+	Kind       string `json:"kind,omitempty" protobuf:"bytes,2,opt,name=kind,proto3"`
+	Name       string `json:"name,omitempty" protobuf:"bytes,3,opt,name=name,proto3"`
+	Uid        string `json:"uid,omitempty" protobuf:"bytes,4,opt,name=uid,proto3"`
 }
 
 // Value return json value, implement driver.Valuer interface
@@ -76,32 +76,32 @@ func (m *OwnerReference) GormDBDataType(db *gorm.DB, field *schema.Field) string
 // +gogo:gengorm=true
 type EntityMeta struct {
 	// 资源名称
-	Name string `json:"name" protobuf:"bytes,1,opt,name=name" gorm:"column:name"`
+	Name string `json:"name" protobuf:"bytes,1,opt,name=name,proto3"`
 	// 资源 id
 	// +primaryKey
 	// +gen:primaryKey
-	Uid int64 `json:"uid" protobuf:"varint,2,opt,name=uid" gorm:"column:uid;primaryKey"`
+	Uid int64 `json:"uid" protobuf:"varint,2,opt,name=uid,proto3"`
 	// 资源版本信息
-	ResourceVersion string `json:"resourceVersion" protobuf:"bytes,3,opt,name=resourceVersion" gorm:"column:resourceVersion"`
+	ResourceVersion string `json:"resourceVersion" protobuf:"bytes,3,opt,name=resourceVersion,proto3"`
 	// 资源描述信息
-	Description string `json:"description" protobuf:"bytes,4,opt,name=description" gorm:"column:description"`
+	Description string `json:"description" protobuf:"bytes,4,opt,name=description,proto3"`
 	// 资源命名空间
-	Namespace string `json:"namespace" protobuf:"bytes,5,opt,name=namespace" gorm:"column:namespace"`
+	Namespace string `json:"namespace" protobuf:"bytes,5,opt,name=namespace,proto3"`
 	// 资源创建时间
-	CreationTimestamp int64 `json:"creationTimestamp" protobuf:"varint,6,opt,name=creationTimestamp" gorm:"column:creationTimestamp"`
+	CreationTimestamp int64 `json:"creationTimestamp" protobuf:"varint,6,opt,name=creationTimestamp,proto3"`
 	// 资源更新时间
-	UpdateTimestamp int64 `json:"updateTimestamp" protobuf:"varint,7,opt,name=updateTimestamp" gorm:"column:updateTimestamp"`
+	UpdateTimestamp int64 `json:"updateTimestamp" protobuf:"varint,7,opt,name=updateTimestamp,proto3"`
 	// 删除时间
-	DeletionTimestamp int64  `json:"deletionTimestamp" protobuf:"varint,8,opt,name=deletionTimestamp" gorm:"column:deletionTimestamp"`
-	GenerateName      string `json:"generateName" protobuf:"bytes,9,opt,name=generateName" gorm:"column:generateName"`
+	DeletionTimestamp int64  `json:"deletionTimestamp" protobuf:"varint,8,opt,name=deletionTimestamp,proto3"`
+	GenerateName      string `json:"generateName" protobuf:"bytes,9,opt,name=generateName,proto3"`
 	// 资源标签
-	Labels dao.Map[string, string] `json:"labels" protobuf:"bytes,10,rep,name=labels" gorm:"column:labels;serializer:json"`
+	Labels dao.Map[string, string] `json:"labels" protobuf:"bytes,10,rep,name=labels,proto3,casttype=github.com/vine-io/apimachinery/storage/dao.Map"`
 	// 资源注解
 	// 只读
-	Annotations dao.Map[string, string] `json:"annotations" protobuf:"bytes,11,rep,name=annotations" gorm:"column:annotations;serializer:json"`
+	Annotations dao.Map[string, string] `json:"annotations" protobuf:"bytes,11,rep,name=annotations,proto3,casttype=github.com/vine-io/apimachinery/storage/dao.Map"`
 	// 资源关系信息
 	// 该资源示例能稳定运行的依赖资源
-	References dao.JSONArray[*OwnerReference] `json:"references" protobuf:"bytes,12,rep,name=references" gorm:"column:references;serializer:json"`
+	References dao.JSONArray[*OwnerReference] `json:"references" protobuf:"bytes,12,rep,name=references,proto3"`
 }
 
 // +gogo:deepcopy=true
@@ -109,32 +109,32 @@ type EntityMeta struct {
 // +gogo:gengorm=true
 type ObjectMeta struct {
 	// 资源名称
-	Name string `json:"name" protobuf:"bytes,1,opt,name=name" gorm:"column:name"`
+	Name string `json:"name" protobuf:"bytes,1,opt,name=name,proto3"`
 	// 资源 id
 	// +primaryKey
 	// +gen:primaryKey
-	Uid string `json:"uid" protobuf:"bytes,2,opt,name=uid" gorm:"column:uid;primaryKey"`
+	Uid string `json:"uid" protobuf:"bytes,2,opt,name=uid,proto3"`
 	// 资源版本信息
-	ResourceVersion string `json:"resourceVersion" protobuf:"bytes,3,opt,name=resourceVersion" gorm:"column:resourceVersion"`
+	ResourceVersion string `json:"resourceVersion" protobuf:"bytes,3,opt,name=resourceVersion,proto3"`
 	// 资源描述信息
-	Description string `json:"description" protobuf:"bytes,4,opt,name=description" gorm:"column:description"`
+	Description string `json:"description" protobuf:"bytes,4,opt,name=description,proto3"`
 	// 资源命名空间
-	Namespace string `json:"namespace" protobuf:"bytes,5,opt,name=namespace" gorm:"column:namespace"`
+	Namespace string `json:"namespace" protobuf:"bytes,5,opt,name=namespace,proto3"`
 	// 资源创建时间
-	CreationTimestamp int64 `json:"creationTimestamp" protobuf:"varint,6,opt,name=creationTimestamp" gorm:"column:creationTimestamp"`
+	CreationTimestamp int64 `json:"creationTimestamp" protobuf:"varint,6,opt,name=creationTimestamp,proto3"`
 	// 资源更新时间
-	UpdateTimestamp int64 `json:"updateTimestamp" protobuf:"varint,7,opt,name=updateTimestamp" gorm:"column:updateTimestamp"`
+	UpdateTimestamp int64 `json:"updateTimestamp" protobuf:"varint,7,opt,name=updateTimestamp,proto3"`
 	// 删除时间
-	DeletionTimestamp int64  `json:"deletionTimestamp" protobuf:"varint,8,opt,name=deletionTimestamp" gorm:"column:deletionTimestamp"`
-	GenerateName      string `json:"generateName" protobuf:"bytes,9,opt,name=generateName" gorm:"column:generateName"`
+	DeletionTimestamp int64  `json:"deletionTimestamp" protobuf:"varint,8,opt,name=deletionTimestamp,proto3"`
+	GenerateName      string `json:"generateName" protobuf:"bytes,9,opt,name=generateName,proto3"`
 	// 资源标签
-	Labels dao.Map[string, string] `json:"labels" protobuf:"bytes,10,rep,name=labels" gorm:"column:labels;serializer:json"`
+	Labels dao.Map[string, string] `json:"labels" protobuf:"bytes,10,rep,name=labels,proto3,casttype=github.com/vine-io/apimachinery/storage/dao.Map"`
 	// 资源注解
 	// 只读
-	Annotations dao.Map[string, string] `json:"annotations" protobuf:"bytes,11,rep,name=annotations" gorm:"column:annotations;serializer:json"`
+	Annotations dao.Map[string, string] `json:"annotations" protobuf:"bytes,11,rep,name=annotations,proto3,casttype=github.com/vine-io/apimachinery/storage/dao.Map"`
 	// 资源关系信息
 	// 该资源示例能稳定运行的依赖资源
-	References dao.JSONArray[*OwnerReference] `json:"references" protobuf:"bytes,12,rep,name=references" gorm:"column:references;serializer:json"`
+	References dao.JSONArray[*OwnerReference] `json:"references" protobuf:"bytes,12,rep,name=references,proto3"`
 }
 
 // Value return json value, implement driver.Valuer interface
@@ -155,17 +155,17 @@ func (m *ObjectMeta) GormDBDataType(db *gorm.DB, field *schema.Field) string {
 // +gogo:deepcopy=true
 // +gogo:genproto=true
 type ListMeta struct {
-	ResourceVersion string `json:"resourceVersion,omitempty" protobuf:"bytes,1,opt,name=resourceVersion"`
-	Page            int32  `json:"page,omitempty" protobuf:"varint,2,opt,name=page"`
-	Size            int32  `json:"size,omitempty" protobuf:"varint,3,opt,name=size"`
-	Total           int64  `json:"total,omitempty" protobuf:"varint,4,opt,name=total"`
+	ResourceVersion string `json:"resourceVersion,omitempty" protobuf:"bytes,1,opt,name=resourceVersion,proto3"`
+	Page            int32  `json:"page,omitempty" protobuf:"varint,2,opt,name=page,proto3"`
+	Size            int32  `json:"size,omitempty" protobuf:"varint,3,opt,name=size,proto3"`
+	Total           int64  `json:"total,omitempty" protobuf:"varint,4,opt,name=total,proto3"`
 }
 
 // +gogo:deepcopy=true
 // +gogo:genproto=true
 // 资源状态
 type State struct {
-	Code StatusCode `json:"code,omitempty" protobuf:"varint,1,opt,name=code"`
+	Code StatusCode `json:"code,omitempty" protobuf:"varint,1,opt,name=code,proto3,casttype=StatusCode"`
 	// code != 0 时，显示错误信息
-	Message string `json:"message,omitempty" protobuf:"bytes,2,opt,name=message"`
+	Message string `json:"message,omitempty" protobuf:"bytes,2,opt,name=message,proto3"`
 }
